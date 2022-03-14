5634ecde57656447177fad0cba0040fc00afd790
LijeshPJaison <lijeshpjaison7@gmail.com>
2022-03-14 16:39:32 +0530

    stfiles

--- dron/settings.py
+++ dron/settings.py
@@ -119,10 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-staticfiles_DIRS=(
-    os.path.join(BASE_DIR,'static')
-)
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
