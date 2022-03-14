11b5f188b4da04fe8330a376dd99b387ee097602
LijeshPJaison <lijeshpjaison7@gmail.com>
2022-03-14 15:36:01 +0530

    change

--- dronapp/urls.py
+++ dronapp/urls.py
@@ -2,6 +2,6 @@ from django import views
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('',views.index,),
-    path('',views.index2),
+    path('index',views.index,),
+    path('index2',views.index2),
 ]
